f9380b66a8fd599f6a4cc8793612bd4f852e3fbb
ColeyG <cjgeerts@gmail.com>
2019-12-08 14:39:04 -0500

    Update Package | Electron Management

---
+++ electron.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 625,
+    minWidth: 100,
+    minHeight: 100,
+    x: 200,
+    y: 100,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('public/index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
