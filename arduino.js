5546fd1a8a1c15a03b543c01497cf0b0a655280a
ColeyG <cjgeerts@gmail.com>
2019-12-11 19:12:02 -0500

    Formatting

--- arduino.js
+++ arduino.js
@@ -4,23 +4,36 @@ const board = new five.Board();
 let temp;
 let photoresistor;
 let moisture;
+
 board.on('ready', () => {
   temp = new five.Sensor({
     pin: 'A4',
     frequency: 250,
-  }); photoresistor = new five.Sensor({
+  });
+
+  photoresistor = new five.Sensor({
     pin: 'A2',
     frequency: 250,
-  }); moisture = new five.Sensor({
+  });
+
+  moisture = new five.Sensor({
     pin: 'A0',
     frequency: 250,
-  }); function changeTemp() {
+  });
+
+  function changeTemp() {
     console.log(this.value);
-  } function changeLight() {
+  }
+
+  function changeLight() {
     console.log(this.value);
-  } function changeMoisture() {
+  }
+
+  function changeMoisture() {
     console.log(this.value);
-  } temp.on('data', changeTemp);
+  }
+
+  temp.on('data', changeTemp);
   photoresistor.on('data', changeLight);
   moisture.on('data', changeMoisture);
 });
